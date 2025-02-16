392ce1e27c543595f7f65cf2cd52735013b3e352
Bomuratov <antikko99@gmail.com>
2025-02-16 17:40:29 +0500

    added dotenv

--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -6,10 +6,10 @@ class Basemodel(models.Model):
     created_time = models.DateTimeField(auto_now_add=True, editable=False, null=True)
     update_time = models.DateTimeField(auto_now=True, editable=False, null=True)
     created_by = models.ForeignKey(
-        "authentication.Usermodel", models.SET_NULL, null=True, blank=True, related_name="created_%(model_name)ss"
+        "authentication.Usermodel", models.SET_NULL, null=True, blank=True, related_name="%(app_label)s_created_%(class)s"
     )
     update_by = models.ForeignKey(
-        "authentication.Usermodel", models.SET_NULL, null=True, blank=True, related_name="updated_%(model_name)ss"
+        "authentication.Usermodel", models.SET_NULL, null=True, blank=True, related_name="%(app_label)s_updated_%(class)s"
     )
     is_active = models.BooleanField(default=False)
 
